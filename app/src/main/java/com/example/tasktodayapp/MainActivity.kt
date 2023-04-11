2a36eb8c4ea7e4768483d83d8cbdcc14d753036b
GabrielMinzon <112291378+GabrielMinzon@users.noreply.github.com>
2023-04-11 20:34:35 -0300

    atualização
    inclusao do menu, e dos icones

--- app/src/main/java/com/example/tasktodayapp/MainActivity.kt
+++ app/src/main/java/com/example/tasktodayapp/MainActivity.kt
@@ -1,38 +1,81 @@
 package com.example.tasktodayapp
 
 import android.os.Bundle
+import android.telecom.Call.Details
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tasktodayapp.ui.theme.TaskTodayAPPTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreenContent()
+            MainScreenContent(drawerState = DrawerState(initialValue = DrawerValue.Closed))
         }
     }
 }
 
 @Composable
-fun MainScreenContent() {
+fun MainScreenContent(drawerState: DrawerState) {
+    val scaffoldState = rememberScaffoldState(drawerState = drawerState)
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
                 TopAppBar(
-                    content = { Text(text = "TaskAppBar")}
+                    title = { Text(text = "TaksTodayApp")},
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                CoroutineScope(Dispatchers.Default).launch {
+                                    scaffoldState.drawerState.open()
+                                }
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Drawer Menu"
+                            )
+                        }
+                    }
                 )
+        },
+        drawerBackgroundColor = Color.Red,
+        drawerGesturesEnabled = drawerState.isOpen,
+        drawerContent = {
+            Box(
+                modifier = Modifier
+                    .background(Color.Magenta)
+                    .height(16.dp)
+            ){
+                Text(text = "Opções!!!")
+            }
+            Column() {
+                Text(text = "Opção de Menu 1")
+                Text(text = "Opção de Menu 2")
+                Text(text = "Opção de Menu 3")
+            }
         },
         content = {
                 paddingValues -> Log.i("paddinValues", "$paddingValues")
@@ -42,9 +85,20 @@ fun MainScreenContent() {
                         .fillMaxSize()
                 ) {
                     MySearchField(modificador = Modifier.fillMaxWidth())
-                    MyTaskWidget(modificador = Modifier.fillMaxWidth())
-                    Text("Task3")
-                    Text("Task4")
+                    MyTaskWidget(
+                        modificador = Modifier.fillMaxWidth(),
+                        taskName = "Preparar Aula LazyList/LazyColum",
+                        taskDetails = "é bem melhor usar lazilist ao inves de colum",
+                        deadEndDate = Date()
+                    )
+                    MyTaskWidget(
+                        modificador = Modifier.fillMaxWidth(),
+                        taskName = "Prova Matematica",
+                        taskDetails = "Estudar Calculo Capitulo 1 e 2",
+                        deadEndDate = Date()
+                    )
+                    //Text("Task3")
+                    //Text("Task4")
                 }
         },
         bottomBar = {
@@ -71,14 +125,50 @@ fun MySearchField(modificador: Modifier){
 }
 
 @Composable
-fun MyTaskWidget(modificador: Modifier){
-    Column(modifier = modificador) {
-        Text(text = "Tarefa X")
+fun MyTaskWidget(
+        modificador: Modifier,
+        taskName: String,
+        taskDetails: String,
+        deadEndDate: Date
+    ){
+    val dateFormatter = SimpleDateFormat("EEE, MMM dd, yyyy", Locale.getDefault())
+    Row(modifier = modificador) {
+        Column() {
+            Icon(
+                imageVector = Icons.Default.Notifications,
+                contentDescription = "Icons of a pendent task"
+            )
+            Text(
+                text = dateFormatter.format(deadEndDate),
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                fontSize = 12.sp
+            )
+        }
+        Column(
+            modifier = modificador
+                .border(width = 1.dp, color = Color.Black)
+                .padding(3.dp)
+        ) {
+            Text(
+                text = taskName,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic
+            )
+            Text(
+                text = taskDetails,
+                fontSize = 10.sp,
+                fontWeight = FontWeight.Normal,
+                fontStyle = FontStyle.Normal
+            )
+        }
     }
+    Spacer(modifier = Modifier.height(16.dp))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MainScreenContent()
+    MainScreenContent(drawerState = DrawerState(initialValue = DrawerValue.Closed))
 }
